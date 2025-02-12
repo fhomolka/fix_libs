97d8ab378e43da25a8f648c6a40a33787ec12165
Fix <fhomolka@protonmail.com>
2025-02-12 11:40:23 +0100

    `fix_swapback` re-align len and maxlen

--- fix_swapback.h
+++ fix_swapback.h
@@ -10,7 +10,7 @@
 #ifndef FIX_SWAPBACK_H
 #define FIX_SWAPBACK_H
 
-#define fix_swapback(T, size) struct { unsigned int len; T data[size]; unsigned int maxlen; }
+#define fix_swapback(T, size) struct { unsigned int maxlen; unsigned int len; T data[size]; }
 #define fix_swapback_init(arr, size) {arr.len = 0; arr.maxlen = size;}
 #define fix_swapback_append(arr, new) do{if(arr.len == arr.maxlen) {break;} arr.data[arr.len] = new; arr.len += 1;}while(0)
 #define fix_swapback_remove(arr, idx) do{if(arr.len < idx){break;} arr.data[idx] = arr.data[arr.len - 1]; arr.len -= 1; }while(0)
